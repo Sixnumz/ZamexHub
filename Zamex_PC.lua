3cd27d5b650ec0d8856ca529c003158b3f7ace3d
Sixnumz <115585031+Sixnumz@users.noreply.github.com>
2023-01-15 17:18:12 +0700

    Update Zamex_PC.lua

--- Zamex_PC.lua
+++ Zamex_PC.lua
@@ -3,6 +3,8 @@ local PlaceId = game.PlaceId
 if PlaceId == 3101667897 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Sixnumz/ZamexHub/main/Zamex_LOS.lua'))()
     warn('Execute success')
+elseif PlaceId == 2753915549 or 4442272183 or 7449423635 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Sixnumz/ZamexHub/main/Zamex_BF.lua'))()
 else
     game.Players.LocalPlayer:Kick('Not Support this map.')
 end
